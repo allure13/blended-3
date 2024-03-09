91232383bf9148325f696d10f5f6902b1b66063b
Myroslava <mir_ok@ukr.net>
2024-03-09 13:34:05 +0200

    task

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,13 +1,49 @@
-// import { FiSearch } from 'react-icons/fi';
+import { useState } from 'react';
+import { FiSearch } from 'react-icons/fi';
+import styles from './SearchForm.module.css';
 
-// const regions = [
-//   { id: 'africa', value: 'africa', name: 'Africa' },
-//   { id: 'america', value: 'america', name: 'America' },
-//   { id: 'asia', value: 'asia', name: 'Asia' },
-//   { id: 'europe', value: 'europe', name: 'Europe' },
-//   { id: 'oceania', value: 'oceania', name: 'Oceania' },
-// ];
+const regions = [
+  { id: 'africa', value: 'africa', name: 'Africa' },
+  { id: 'america', value: 'america', name: 'America' },
+  { id: 'asia', value: 'asia', name: 'Asia' },
+  { id: 'europe', value: 'europe', name: 'Europe' },
+  { id: 'oceania', value: 'oceania', name: 'Oceania' },
+];
 
-export const SearchForm = () => {
-  return <h2>SearchForm</h2>;
+export const SearchForm = ({onHandleSubmit}) => {
+  const [query, setQuery] = useState('');
+  const handleChange = (e) => {
+    setQuery(e.target.value)}
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!query.trim()) return;
+    onHandleSubmit(query)
+  }
+
+
+
+  return <form className={styles.form} onSubmit={handleSubmit}>
+  <button className={styles.button} type="submit">
+    <FiSearch size="16px" />
+  </button>
+
+  <select
+    aria-label="select"
+    className={styles.select}
+    name="region"
+    required
+    value={query}
+    onChange={handleChange}
+  >
+    <option disabled value="">
+      Select a region
+    </option>
+    {regions.map(({id, name, value}) => (<option value={value} key={id}>{name}</option>
+))}
+  </select>
+</form>
+;
 };
+
+
